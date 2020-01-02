387e370b485a7d139a1392960ee30871822ffbbc
dobis32 <scottvanderweide32@gmail.com>
2020-01-02 18:32:56 -0500

    initj

---
+++ app/index.js
@@ -0,0 +1,48 @@
+console.log('hello there');
+
+const adjustDays = function(numberOfDays) {
+	let days = document.querySelector('#day');
+	while (days.firstChild) {
+		days.removeChild(days.firstChild);
+	}
+	const none = document.createElement('option');
+	none.value = 'none';
+	none.innerHTML = '-- Day --';
+	days.appendChild(none);
+	for (let i = 1; i <= numberOfDays; i++) {
+		const day = document.createElement('option');
+		day.value = i;
+		day.innerHTML = i;
+		days.appendChild(day);
+	}
+};
+
+const monthChange = function(event) {
+	const month = event.target.value;
+	switch (month) {
+		case 'JANUARY':
+			adjustDays(31);
+		case 'FEBRUARY':
+			adjustDays(28);
+		case 'MARCH':
+			adjustDays(31);
+		case 'APRIL':
+			adjustDays(30);
+		case 'MAY':
+			adjustDays(31);
+		case 'JUNE':
+			adjustDays(30);
+		case 'JULY':
+			adjustDays(31);
+		case 'AUGUST':
+			adjustDays(31);
+		case 'SEPTEMBER':
+			adjustDays(30);
+		case 'OCTOBER':
+			adjustDays(31);
+		case 'NOVEMBER':
+			adjustDays(30);
+		case 'DECEMBER':
+			adjustDays(31);
+	}
+};
